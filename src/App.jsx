435f90f01397aba03401040ed9fc94c641d95b83
Siddharth Ruria <siddharthruria10@gmail.com>
2025-07-02 20:39:57 +0530

    Feature/navbar (#2)
    * feat:navbar-component-created
    
    * feat:navbar-component-completed
    
    ---------
    
    Co-authored-by: Prajeet Singh Tomar <prajeetsinghtomar@Prajeets-MacBook-Pro.local>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Navbar from './components/navbar/navbar';
+import './App.css';
+
+
 
 function App() {
   return (
     <>
+      <div className=''>
+       <Navbar/>
+       <div className="content">
+        <h1>Welcome to the Website</h1>
+        <p>Scroll down to see the logo shrink and navbar appear.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      </div>
     </>
   );
 }
-
+ 
 export default App;
