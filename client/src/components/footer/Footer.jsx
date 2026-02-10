22b9f5fceb3b8f946f203309e96f2146bada9b49
Prajeet Tomar <prsingh@perion.com>
2026-02-10 19:27:14 +0530

    build-changes

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './footer.module.scss';
+import styles from './Footer.module.scss';
 import DesireHubLogo from '../../assets/images/Desirehublogo.png';
 
 const Footer = () => {
