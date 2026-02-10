e7ea402170ad8e782226f87896e120741432f327
Prajeet Tomar <prsingh@perion.com>
2026-02-10 19:23:43 +0530

    build-error-solved

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import Layout from "./layouts/layout.jsx";
+import Layout from "./layouts/Layout.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
